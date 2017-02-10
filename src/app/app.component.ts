c318af064b7fbf8f2354f96b3bb1cd88894e4145
neclodiver <neclodiver@live.com>
2017-02-10 17:31:48 +0800

    进行代码的调整和继续深入

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,14 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-}
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor='let hero of heroes'
@@ -86,14 +66,22 @@ const HEROES: Hero[] = [
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }`
-  ]
-
+  ],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  constructor(private heroService: HeroService) { }
+
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 }
