411233d62947c1f824b07be9b1e4468c0e5f35e2
necrodiver <neclodiver@live.com>
2017-02-13 15:14:27 +0800

    增加内容，添加文件，使之更为规范

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+
+RouterModule.forRoot([{
+  path: 'heroes',
+  component: HeroesComponent
+}])
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([{
+      path: 'heroes',
+      component: HeroesComponent
+    }, {
+      path: 'dashboard',
+      component: DashboardComponent
+    }, {
+      path: '',
+      redirectTo: '/dashboard',
+      pathMatch: 'full'
+    }, {
+      path: 'detail/:id',
+      component: HeroDetailComponent
+    }])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
+  providers: [
+    HeroService
+  ]
+  ,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
