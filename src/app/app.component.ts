8df272379c244eaf94df7121d30bfcea1ddfad23
necrodiver <neclodiver@live.com>
2017-02-14 15:43:40 +0800

    完成搜索功能的学习

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId:module.id,
+    moduleId: module.id,
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
         </nav>
         <router-outlet></router-outlet>
     `,
-    styleUrls:['app.component.css']
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
     title = "Tour of Heroes";
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import './rxjs-extensions';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -8,6 +10,7 @@ import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -26,7 +29,8 @@ import { InMemoryDataService } from './in-memory-data.service';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
