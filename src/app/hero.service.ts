266c96964416f06b48cc7b940694ea4bf2434cde
necrodiver <neclodiver@live.com>
2017-02-14 14:06:18 +0800

    添加增删改操作

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,7 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
     private heroesUrl = 'app/heroes';
+    private headers = new Headers({ 'Content-Type': 'application/json' });
     constructor(private http: Http) { }
     getHeroes(): Promise<Hero[]> {
         return this.http.get(this.heroesUrl)
@@ -27,5 +28,28 @@ export class HeroService {
         return new Promise<Hero[]>(resolve =>
             setTimeout(resolve, 2000))
             .then(() => this.getHeroes());
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http.put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, { headers: this.headers })
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+
     }
 }
